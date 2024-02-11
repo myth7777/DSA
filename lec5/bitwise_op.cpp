95130d9679607c57d8117cc779a6aa05a27dd574
myth7777 <101268873+myth7777@users.noreply.github.com>
2024-02-11 14:30:59 +0530

    Add files via upload

---
+++ lec5/bitwise_op.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=4,b=6;
+    cout<<(a&b)<<endl; //output will be 4
+    cout<<(a|b)<<endl; //output will be 6
+    cout<<(~a)<<endl; //output will be -5
+    cout<<(a^b)<<endl; //output will be 2
+    cout<<endl;
+
+    cout<<(3<<2)<<endl; //output will be 12 (left shift)
+    cout<<(17>>2)<<endl; //output will be 4 (right shift)
+}
